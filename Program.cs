53546de22a2897242aa00a51d2cad4f432fc23a0
gebz_ <aly.ghobashy@sharetech-me.com>
2023-05-29 15:23:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+using Radzen.Blazor;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Vitalis.Shared.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+builder.Services.AddSingleton<ThemeService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Shared/Services/ThemeService.cs
@@ -0,0 +1,62 @@
+﻿using MudBlazor;
+
+namespace Vitalis.Shared.Services
+{
+    public class ThemeService
+    {
+        public MudTheme CurrentTheme { get; private set; }
+
+        public bool IsDarkMode { get; private set; }
+
+        public event Action ThemeChanged;
+        private MudTheme _lightTheme;
+        private MudTheme _darkTheme;
+
+        public ThemeService()
+        {
+            // Define the light theme
+            _lightTheme = new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Primary = "#1976d2",
+                    Secondary = "#ff4081",
+                    Background = "#f5f5f5",
+                    AppbarBackground = "#ffffff",
+                    DrawerBackground = "#f5f5f5",
+                    DrawerText = "rgba(0,0,0, 0.87)",
+                    TextPrimary = "rgba(0,0,0, 0.87)",
+                    TextSecondary = "rgba(0,0,0, 0.54)"
+                }
+            };
+
+            // Define the dark theme
+            _darkTheme = new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Primary = Colors.Blue.Lighten3,
+                    Secondary = Colors.DeepPurple.Accent2,
+                    Background = Colors.Grey.Darken4,
+                    AppbarBackground = Colors.Grey.Darken3,
+                    DrawerBackground = Colors.Grey.Darken4,
+                    DrawerText = "rgba(255,255,255, 0.87)",
+                    TextPrimary = "rgba(255,255,255, 0.87)",
+                    TextSecondary = "rgba(255,255,255, 0.54)"
+                }
+            };
+
+            // Set the initial theme
+            IsDarkMode = false;
+            CurrentTheme = _lightTheme;
+        }
+
+
+        public void ToggleDarkMode()
+        {
+            IsDarkMode = !IsDarkMode;
+            CurrentTheme = IsDarkMode ? _darkTheme : _lightTheme;
+            ThemeChanged?.Invoke();
+        }
+    }
+}
